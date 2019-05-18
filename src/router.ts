6840e5a9cbd382962e79977b4ebe9dfd95d65c27
millionkn <millionkn@sina.com>
2019-05-18 18:51:35 +0900

    去掉一处无用引入

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Empty from "@/components/Empty.vue";
-import VueRouter from 'vue-router';
+
 Vue.use(Router);
 export default new Router({
   mode: 'history',
