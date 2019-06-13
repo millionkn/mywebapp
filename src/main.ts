2d25aa2af24051ea4cd19529fd29b6774277f430
millionkn <millionkn@sina.com>
2019-06-13 22:28:40 +0800

    测试数据移动到自带的服务器

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
+import "amazeui/dist/css/amazeui.css";
+import "amazeui/less/amazeui.less";
+import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import "amazeui/less/amazeui.less";
-import "amazeui/dist/css/amazeui.css";
-import 'element-ui/lib/theme-chalk/index.css';
-import env from './env';
-env.then(() => {
-  Vue.config.productionTip = false;
-  new Vue({
-    store,
-    router,
-    render: h => h(App)
-  }).$mount('#app')
-})
+Vue.config.productionTip = false;
+new Vue({
+  store,
+  router,
+  render: h => h(App)
+}).$mount('#app')
 
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,62 @@
+let moment = require('moment');
 let path = require("path");
-module.exports={
-    css:{
-        loaderOptions:{
-            less:{
+let bodyParser = require('body-parser');
+module.exports = {
+    css: {
+        loaderOptions: {
+            less: {
                 paths: [path.resolve(__dirname, 'node_modules')],
             }
         }
     },
-    devServer:{
-        port:3000,
+    devServer: {
+        port: 3000,
+        before(app, server) {
+            app.use(bodyParser.urlencoded({ extended: false }));
+            app.use(bodyParser.json());
+            app.post('/login', (req, res) => {
+                if (req.body.username === 'admin' && req.body.password === '12345678') {
+                    res.send({
+                        name: "测试用户",
+                        role: "测试者",
+                    })
+                } else {
+                    res.sendStatus(403)
+                }
+            });
+            app.post('/loginOut', (req, res) => { res.send(undefined) });
+            app.get('/data/console', (req, res) => {
+                res.send([
+                    {
+                        office: "科室A",
+                        name: "仪器A1",
+                        buyDate: new Date().valueOf(),
+                        lastCheck: new Date().valueOf(),
+                        inspectionTimes: 7,
+                        line: 3,
+                    },
+                    {
+                        office: "科室A",
+                        name: "仪器A1",
+                        buyDate: new Date().valueOf(),
+                        lastCheck: moment().add({
+                            day: -4,
+                        }).valueOf(),
+                        inspectionTimes: 7,
+                        line: 3,
+                    },
+                    {
+                        office: "科室B",
+                        name: "仪器B1",
+                        buyDate: new Date().valueOf(),
+                        lastCheck: moment().add({
+                            day: -6,
+                        }).valueOf(),
+                        inspectionTimes: 7,
+                        line: 5,
+                    },
+                ])
+            });
+        }
     }
 }
